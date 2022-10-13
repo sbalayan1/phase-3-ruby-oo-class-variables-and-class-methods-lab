503bf388d112d1674bc37ce603ea5a590b099add
sbalayan1 <balayans2014@yahoo.com>
2022-10-13 04:11:58 -0700

    commit tree 9ea202820c8cdcbfd1721ea9f99d69bd3db53e40

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize name, artist, genre
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@artists << artist
+        @@count += 1
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+end
